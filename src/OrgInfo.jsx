e855dde1e5e15b2c9e4a6fe1881e30fef7fc27ff
Spencer Wyman <spencer.wyaman7@gmail.com>
2020-02-20 09:50:10 -0800

    made Org Info comp w/o state info

---
+++ src/OrgInfo.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function OrgInfo(props) {
+
+  return (
+    <div>
+      <p>Org name</p>
+      <p>Description</p>
+      <p>Email</p>
+    </div>
+  )
+}
+
+export default OrgInfo;
