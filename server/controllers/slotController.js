e162035163458ae13fac43b528b0b0679656fb4c
Arman Gurkan <arman@secelektrik.com>
2020-02-20 16:02:34 -0800

    delt with slotcontrollers

--- server/controllers/slotController.js
+++ server/controllers/slotController.js
@@ -38,11 +38,40 @@ slotController.deleteSlot = (req, res, next) => {
 	})
 }
 
-slotController.getSlots = ()
-
-module.exports = {
-	// updateSlot,
-	deleteSlot,
-	createSlot
-	// getSlots
+slotController.getSlots = async (req, res, next) => {
+	try{
+		const {filterElement, filterId, returnElements} = req.params;
+		console.log(filterElement, filterId, returnElements);
+		if (filterElement && filterId) {
+			console.log('filtered get route');
+			const queryText = `SELECT * FROM slots where `.concat(String(filterElement), ' = $1', );
+			if (returnElements) {
+				console.log('also return modified get route');
+				const queryTextAdd = returnElements.split('&').join(', ');
+				queryText.replace('*', queryTextAdd);
+			}
+			const query = {
+				text: queryText,
+				values: [String(filterId)],
+				rowMode: 'array',
+			}
+			console.log('i am about to run the aaaaaaa for getting Orgteers with', queryText);
+			const response = await db.query(query);
+			console.log('these are the rows', response);
+			res.locals.data = response.rows[0];
+			return next();
+		}
+		console.log('plain get route');
+		const queryText = 'SELECT * FROM slots;';
+		const { rows } = await db.query(queryText, []);
+		console.log(rows);
+		res.locals.data = [...rows];
+		return next();
+	}
+	catch(error){
+		await db.query( 'ROLLBACK' );
+		return next(error);
+	}
 }
+
+module.exports = slotController;
